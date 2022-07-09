6de72a8ca54c37644386c4e61b02bc2ec27d0c31
Yoochanhong <chanhongy6@naver.com>
2022-07-09 21:09:27 +0900

    백준 10430번

---
+++ 10430.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	if (2 <= a, b, c <= 10000) {
+		printf("%d\n", (a + b) % c);
+		printf("%d\n", ((a % c) + (b % c)) % c);
+		printf("%d\n", (a * b) % c);
+		printf("%d", ((a % c) * (b % c)) % c);
+	}
+	return 0;
+}

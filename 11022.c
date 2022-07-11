6d91520d8e867b649e19f29a378130ac34ccacd8
Yoochanhong <chanhongy6@naver.com>
2022-07-11 11:19:38 +0900

    백준 11022번

---
+++ 11022.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+	int a, b, t;
+	scanf("%d", &t);
+	for (int i = 1; i <= t; i++) {
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+	}
+	return 0;
+}

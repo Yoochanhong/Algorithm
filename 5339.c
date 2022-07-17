18489ec03b160d79618da00cba4101f68547812f
Yoochanhong <chanhongy6@naver.com>
2022-07-17 14:41:37 +0900

    백준 5339번

---
+++ 5339.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("     /~\\\n");
+    printf("    ( oo|\n");
+    printf("    _\\=/_\n");
+    printf("   /  _  \\\n");
+    printf("  //|/.\\|\\\\\n");
+    printf(" ||  \\ /  ||\n");
+    printf("============\n");
+    printf("|          |\n");
+    printf("|          |\n");
+    printf("|          |\n");
+    return 0;
+}

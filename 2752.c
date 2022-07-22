e119f87ffe1d5480a31485c1ae33b417dddbfcc8
Yoochanhong <chanhongy6@naver.com>
2022-07-22 13:10:20 +0900

    백준 2752번

---
+++ 2752.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a < b && a < c) {
+		if (b < c)  printf("%d %d %d", a, b, c);
+		else printf("%d %d %d", a, c, b);
+	}
+	else if (b < c && b < a) {
+		if (a < c) printf("%d %d %d", b, a, c);
+		else printf("%d %d %d", b, c, a);
+	}
+	else if (c < a && c < b) {
+		if (a < b) printf("%d %d %d", c, a, b);
+		else printf("%d %d %d", c, b, a);
+	}
+	return 0;
+}

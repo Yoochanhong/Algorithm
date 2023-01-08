8a755b0fc49f2ce75c6a6676f9640da6f29f4639
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-08 12:59:57 +0900

    백준 1932번

--- 1932.c
+++ 1932.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
 int max(int a, int b) {
-    if (a > b) return a;
-    else return b;
+    return a > b ? a : b;
 }
 
 int main() {
-    int n, arr[501][501], dp[501][501] = {0,}, big = 0;
+    int n, arr[501][501], big = 0;
     scanf("%d", &n);
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
             scanf("%d", &arr[i][j]);
         }
     }
-    dp[1][1] = arr[1][1];
     for (int i = 2; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
-            if (j == 1) dp[i][j] = dp[i - 1][j] + arr[i][j];
-            else if (j == i) dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
-            else dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + arr[i][j];
+            if (j == 1) arr[i][j] += arr[i - 1][j];
+            else if (j == i) arr[i][j] += arr[i - 1][j - 1];
+            else arr[i][j] += max(arr[i - 1][j], arr[i - 1][j - 1]);
         }
     }
     for (int i = 1; i <= n; i++) {
-        if (dp[n][i] > big) big = dp[n][i];
+        if (arr[n][i] > big) big = arr[n][i];
     }
     printf("%d", big);
 }

3dc5ee4ae2d5d6c15ba1854efd2cda2bc1437493
맥북 유찬홍 <chanhongy6@naver.com>
2023-02-05 14:20:52 +0900

    백준 6764번

--- 6764.c
+++ 6764.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 
 int main() {
-  int arr[4], sum = 0;
-  for (int i = 0; i < 4; i++){
-    scanf("%d", &arr[i]);
-  }
-  for (int i = 0; i < 3; i++){
-    if (arr[i] < arr[i+1]) sum++;
-    else if (arr[i] == arr[i+1]){
-      for (int i = 0; i<3; i++){
-        if (arr[i] != arr[i+1]){
-          printf("No Fish");
-          return 0;
-        }
-        else sum +=0;
-      }
+    int arr[4], u = 0, d = 0, s = 0;
+    for (int i = 0; i < 4; i++) {
+        scanf("%d", &arr[i]);
     }
-    else if (arr[i] > arr[i+1])sum--;
-  }
-  if (sum == 3) printf("Fish Rising");
-  else if (sum == -3) printf("Fish Diving");
-  else if (sum == 0) printf("Fish At Constant Depth");
-  return 0;
+    for (int i = 1; i < 4; i++) {
+        if (arr[i] > arr[i - 1]) u++;
+        else if (arr[i] < arr[i - 1]) d++;
+        else s++;
+    }
+    if (u == 3) printf("Fish Rising");
+    else if (d == 3) printf("Fish Diving");
+    else if (s == 3) printf("Fish At Constant Depth");
+    else printf("No Fish");
+    return 0;
 }

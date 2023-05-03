c813ccb10a19347527ac7e3bd5bcb6a8b0d54f37
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2023-05-03 21:00:37 +0900

    브론즈 2

--- 백준/브론즈 2/1009.c
+++ 백준/브론즈 2/1009.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
 int main() {
-
-	int n, a, b;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d %d", &a, &b);
-		int sum = 1;
-		for (int j = 0; j < b; j++) {
-			sum *= a;
-			sum %= 10;
-		}
-
-		if (sum == 0) printf("10\n");
-		else printf("%d\n", sum);
-	}
-
-
-	return 0;
+    int t, a, b;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++) {
+        scanf("%d %d", &a, &b);
+        int a1 = a, c;
+        c = b % 4 == 0 ? 4 : b % 4;
+        for (int j = 1; j < c; j++) {
+            a *= a1;
+        }
+        printf("%d\n", a % 10 == 0 ? 10 : a % 10);
+    }
 }
